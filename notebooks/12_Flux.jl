590e955d8319f2307a38347e606d9343b6ccaf58
Jose Storopoli <thestoropoli@gmail.com>
2021-06-09 18:48:34 -0300

    adicionado ambiente

--- notebooks/12_Flux.jl
+++ notebooks/12_Flux.jl
@@ -14,6 +14,14 @@ begin
 	using PlutoUI
 end
 
+# ╔═╡ 228e9bf1-cfd8-4285-8b68-43762e1ae8c7
+begin
+	using InteractiveUtils
+	with_terminal() do
+		versioninfo()
+	end
+end
+
 # ╔═╡ cbc48ca5-f1a4-4e13-9323-2fd2c43d8612
 TableOfContents(aside=true)
 
@@ -22,7 +30,25 @@ md"""
 # Redes Neurais com `Flux.jl`
 """
 
+# ╔═╡ d548bc1a-2e20-4b7f-971b-1b07faaa4c13
+md"""
+## Ambiente
+"""
+
+# ╔═╡ 23974dfc-7412-4983-9dcc-16e7a3e7dcc4
+with_terminal() do
+	deps = [pair.second for pair in Pkg.dependencies()]
+	deps = filter(p -> p.is_direct_dep, deps)
+	deps = filter(p -> !isnothing(p.version), deps)
+	list = ["$(p.name) $(p.version)" for p in deps]
+	sort!(list)
+	println(join(list, '\n'))
+end
+
 # ╔═╡ Cell order:
 # ╟─cbc48ca5-f1a4-4e13-9323-2fd2c43d8612
 # ╟─7bb67403-d2ac-4dc9-b2f1-fdea7a795329
 # ╠═27f62732-c909-11eb-27ee-e373dce148d9
+# ╟─d548bc1a-2e20-4b7f-971b-1b07faaa4c13
+# ╟─228e9bf1-cfd8-4285-8b68-43762e1ae8c7
+# ╟─23974dfc-7412-4983-9dcc-16e7a3e7dcc4
